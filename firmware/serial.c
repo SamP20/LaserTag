fc2bc1cb678b0e08c989292a9dce2365c3db4dcc
Sam Partridge <sampartr@gmail.com>
2017-05-14 11:28:11 +0100

    serial drivers

---
+++ firmware/serial.c
@@ -0,0 +1,79 @@
+#include "serial.h"
+#include <avr/interrupt.h>
+
+FILE uartfile = FDEV_SETUP_STREAM(uart_putchar, uart_getchar, _FDEV_SETUP_RW);
+
+volatile uint8_t uart_tx_buf[UART_BUF_LEN];
+volatile uint8_t uart_rx_buf[UART_BUF_LEN];
+uint8_t uart_tx_write_ptr = 0;
+uint8_t uart_tx_read_ptr = 0;
+volatile uint8_t uart_tx_len = 0;
+uint8_t uart_rx_write_ptr = 0;
+uint8_t uart_rx_read_ptr = 0;
+volatile uint8_t uart_rx_len = 0;
+
+void uart_init(uint8_t enabled)
+{
+    UBRR0H = (BAUDRATE>>8); // set baud rate
+    UBRR0L = BAUDRATE;
+    UCSR0B = enabled | (1<<RXCIE0); // enable receiver and transmitter
+    UCSR0C = (1<<UCSZ00)|(1<<UCSZ01);   // 8bit data format
+
+}
+
+uint8_t uart_available(void)
+{
+    return uart_rx_len;
+}
+
+int uart_putchar(char var, FILE *stream)
+{
+    while(uart_tx_len>=UART_BUF_LEN);
+    uart_tx_buf[uart_tx_write_ptr++] = (uint8_t)var;
+    uart_tx_write_ptr &= UART_BUF_MASK;
+    uart_tx_len++;
+    UCSR0B |= (1<<UDRIE0); // Enable TX empty interrupt
+    return 0;
+}
+
+int uart_getchar(FILE *stream)
+{
+    while(uart_rx_len==0);
+    uint8_t val = uart_rx_buf[uart_rx_read_ptr++];
+    uart_rx_read_ptr &= UART_BUF_MASK;
+    uart_rx_len--;
+    return val;
+}
+
+ISR(USART_TX_vect)
+{
+}
+
+ISR(USART_RX_vect)
+{
+    if(uart_rx_len<=UART_BUF_LEN)
+    {
+        uart_rx_buf[uart_rx_write_ptr++] = UDR0;
+        uart_rx_write_ptr &= UART_BUF_MASK;
+        uart_rx_len++;
+    }
+    else
+    {
+        // Buffer overun. Nothing we do is good. Just drop the bytes
+        uint8_t tmp = UDR0;
+    }
+}
+
+ISR(USART_UDRE_vect)
+{
+    if(uart_tx_len>0)
+    {
+        UDR0 = uart_tx_buf[uart_tx_read_ptr++];
+        uart_tx_read_ptr &= UART_BUF_MASK;
+        uart_tx_len--;
+    }
+    else
+    {
+        UCSR0B &= ~(1<<UDRIE0); // No more data to send. Disable empty interrupt
+    }
+}
---
+++ firmware/serial.h
@@ -0,0 +1,25 @@
+#ifndef SERIAL_H
+#define SERIAL_H
+
+#include <stdint.h>
+#include <avr/io.h>
+#include <stdio.h>
+
+#define BAUD 9600
+#define BAUDRATE ((F_CPU)/(BAUD*16UL)-1)
+#define UART_RX_EN (1<<RXEN0)
+#define UART_TX_EN (1<<TXEN0)
+
+#define UART_BUF_POWER 5
+#define UART_BUF_LEN (1<<UART_BUF_POWER)
+#define UART_BUF_MASK (UART_BUF_LEN-1)
+
+void uart_init(uint8_t enabled);
+uint8_t uart_available(void);
+
+int uart_putchar(char var, FILE *stream);
+int uart_getchar(FILE *stream);
+
+extern FILE uartfile;
+
+#endif
